cac0eb7d1222412b5197ab65bbdbfb0f07545f7b
Brianna Williams <brianna.williams@gaiaresources.com.au>
2024-04-22 16:42:32 +1000

    Initial work for api upload.

--- src/app/services/upload/upload.service.ts
+++ src/app/services/upload/upload.service.ts
@@ -1,16 +1,26 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from "rxjs";
+import { Observable } from "rxjs";
+import { StorageService } from "../storage/storage.service";
+import { APIService } from "../api/api.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class UploadService {
 
-  constructor() {
+  constructor(
+    private storageService: StorageService,
+    private apiService: APIService,
+  ) {
   }
 
-  upload(): Observable<void> {
-    return of();
+  upload(): Observable<object| null> {
+    return this.storageService.getUploadableRecords().pipe(
+      map(clientRecord => {
+        // TODO need to subscribe? what to do with this?
+        return this.apiService.createRecord(clientRecord).subscribe(data => console.log(data))
+      })
+    );
   }
 
 }
