b229b15c5f31e675c99f3c150e449977fda6f6b6
nick-crawford-gaia <102127192+nick-crawford-gaia@users.noreply.github.com>
2024-05-01 15:14:32 +1000

    [rhys] Restore styling from legacy app

--- src/app/pages/observation/observation.page.ts
+++ src/app/pages/observation/observation.page.ts
@@ -14,17 +14,23 @@ import {
   IonTitle,
   IonToolbar,
   IonFabList,
-  IonImg
+  IonImg,
+  IonLabel
 } from '@ionic/angular/standalone';
+import {FaIconComponent} from "@fortawesome/angular-fontawesome";
+import {faList, faMap} from "@fortawesome/free-solid-svg-icons";
 
 @Component({
   selector: 'app-observation',
   templateUrl: './observation.page.html',
   styleUrls: ['./observation.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonTabBar, IonTabButton, IonTabs, IonFab, IonFabButton, IonFabList, IonImg]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonMenuButton, IonTabBar, IonTabButton, IonTabs, IonFab, IonFabButton, IonFabList, IonImg, FaIconComponent, IonLabel]
 })
 export class ObservationPage implements OnInit {
+
+  public faList = faList;
+  public faMap = faMap;
 
   constructor() {
   }
